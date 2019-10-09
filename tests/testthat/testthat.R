0666fb0b778d3d00f908fcd5a6a04d2825472251
Michael Whitehouse <mw16518@bristol.ac.uk>
2019-10-09 16:51:31 +0100

    test fix

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -1,8 +1,8 @@
 library(testthat)
 library(OneSwap)
 
-test_check("oneswap returns correct response", {
+test_that("oneswap returns correct response", {
   expect_equal(oneswap(c(1,2,3,4,5)), TRUE)
   expect_equal(oneswap(c(2,1,3,4,6,5,7)), FALSE)
-  expect_equal(str_length(c(1,2,7,4,5,6,3,8,9)), TRUE)
+  expect_equal(oneswap(c(1,2,7,4,5,6,3,8,9)), TRUE)
 })
